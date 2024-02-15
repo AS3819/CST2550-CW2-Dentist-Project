fcd1bad4c12126cb5f7c7216f2a691899cd035d3
U-DESKTOP-3I589HK\Adam <AS3819@live.mdx.ac.uk>
2024-02-15 18:59:55 +0000

    Initial tests.

---
+++ Dentist_Programme.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <ctime>
+#include "Print_Functions.h"
+
+int main() {
+
+  std::cout << "Test 1." << std::endl;
+  print("Test 2. \n");
+  print("Test 3. \n");
+  print("Test 4. ");
+  println("Test 5.");
+  println("Test 6.");
+  print(1);
+  
+}
