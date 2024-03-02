0c35f7af68d57677e91c1599db6a379ff33f8a2a
U-DESKTOP-3I589HK\Adam <AS3819@live.mdx.ac.uk>
2024-03-02 16:58:18 +0000

    Added tests.

--- Dentist_Programme.cpp
+++ Dentist_Programme.cpp
@@ -4,8 +4,11 @@
 #include <vector>
 #include <ctime>
 #include "Print_Functions.h"
+#include "Input_Validation.h"
 
 int main() {
+
+  std::string testString = "";
 
   std::cout << "Test 1." << std::endl;
   print("Test 2. \n");
@@ -14,5 +17,33 @@ int main() {
   println("Test 5.");
   println("Test 6.");
   print(1);
+
+  println(testString);
+
+  if (isEmpty(testString)) {
+    println("True.");
+  }
+
+  testString = "1";
+
+  if (!isEmpty(testString)) {
+    println("False.");
+  }
+
+  if (isIntegerString(testString)) {
+    println("True.");
+  }
+
+  testString = "-1";
+
+  if (isIntegerString(testString)) {
+    println("True.");
+  }
+
+  testString = "Test.";
+
+  if (!isIntegerString(testString)) {
+    println("False.");
+  }
   
 }
