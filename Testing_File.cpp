7a9ade1e6d22e1281c4471a5df615f320b6c6b69
Arbaaz Hussain <83524685+arbaazhussain1@users.noreply.github.com>
2024-04-24 16:57:44 +0100

    Add files via upload
    Test with working test cases.

--- Testing_File.cpp
+++ Testing_File.cpp
@@ -6,6 +6,14 @@
 #include "Appointment.h"
 #include "Print_Functions.h"
 #include <chrono>
+#include "AppointmentDisplay.h"
+#include "DentistManager.h"
+#include "AppointmentManager.h"
+#include <fstream>
+#include "DateTimeUtils.h"
+#include "CSVSearcher.h"
+
+
 
 //Declarations.
 
@@ -35,98 +43,74 @@ Appointment appointment5(5, &room2, now, &dentist2, &patient5);
 
 TEST_CASE("dentist1", "[dentist1]")
 {
-  REQUIRE(dentist1.getDentistID() == 1);
-  REQUIRE(dentist1.getTitle() == "Dr.");
-  REQUIRE(dentist1.getFirstName() == "Andrew");
-  REQUIRE(dentist1.getSurname() == "Brown");
-  REQUIRE(dentist1.getAddress() == "address1");
-  REQUIRE(dentist1.getEmail() == "email1");
-  REQUIRE(dentist1.getSalary() == 50000);
+    REQUIRE(dentist1.getDentistID() == 1);
+    REQUIRE(dentist1.getTitle() == "Dr.");
+    REQUIRE(dentist1.getFirstName() == "Andrew");
+    REQUIRE(dentist1.getSurname() == "Brown");
+    REQUIRE(dentist1.getAddress() == "address1");
+    REQUIRE(dentist1.getEmail() == "email1");
+    REQUIRE(dentist1.getSalary() == 50000);
 }
 
 TEST_CASE("dentist2", "[dentist2]")
 {
-  REQUIRE(dentist2.getDentistID() == 2);
-  REQUIRE(dentist2.getTitle() == "Dr.");
-  REQUIRE(dentist2.getFirstName() == "Thomas");
-  REQUIRE(dentist2.getSurname() == "Green");
-  REQUIRE(dentist2.getAddress() == "address2");
-  REQUIRE(dentist2.getEmail() == "email2");
-  REQUIRE(dentist2.getSalary() == 30000);
+    REQUIRE(dentist2.getDentistID() == 2);
+    REQUIRE(dentist2.getTitle() == "Dr.");
+    REQUIRE(dentist2.getFirstName() == "Thomas");
+    REQUIRE(dentist2.getSurname() == "Green");
+    REQUIRE(dentist2.getAddress() == "address2");
+    REQUIRE(dentist2.getEmail() == "email2");
+    REQUIRE(dentist2.getSalary() == 30000);
 }
 
 TEST_CASE("dentist3", "[dentist3]")
 {
-  REQUIRE(dentist3.getDentistID() == 3);
-  REQUIRE(dentist3.getTitle() == "Dr.");
-  REQUIRE(dentist3.getFirstName() == "Peter");
-  REQUIRE(dentist3.getSurname() == "Yeoman");
-  REQUIRE(dentist3.getAddress() == "address3");
-  REQUIRE(dentist3.getEmail() == "email3");
-  REQUIRE(dentist3.getSalary() == 40000);
+    REQUIRE(dentist3.getDentistID() == 3);
+    REQUIRE(dentist3.getTitle() == "Dr.");
+    REQUIRE(dentist3.getFirstName() == "Peter");
+    REQUIRE(dentist3.getSurname() == "Yeoman");
+    REQUIRE(dentist3.getAddress() == "address3");
+    REQUIRE(dentist3.getEmail() == "email3");
+    REQUIRE(dentist3.getSalary() == 40000);
 }
 
-TEST_CASE("patient1", "[patient1]")
-{
-  REQUIRE(patient1.getPatientID() == 1);
-  REQUIRE(patient1.getTitle() == "Mr.");
-  REQUIRE(patient1.getFirstName() == "John");
-  REQUIRE(patient1.getSurname() == "Smith");
-  REQUIRE(patient1.getAddress() == "address1");
-  REQUIRE(patient1.getEmail() == "email1");
+
+TEST_CASE("Patient Initialization") {
+    SECTION("Patient1 Initialization") {
+        REQUIRE(patient1.getPatientID() == 1);
+        REQUIRE(patient1.getTitle() == "Mr.");
+        REQUIRE(patient1.getFirstName() == "John");
+        REQUIRE(patient1.getSurname() == "Smith");
+        REQUIRE(patient1.getAddress() == "address1");
+        REQUIRE(patient1.getEmail() == "email1");
+    }
+
+    SECTION("Patient2 Initialization") {
+        REQUIRE(patient2.getPatientID() == 2);
+        REQUIRE(patient2.getTitle() == "Mr.");
+        REQUIRE(patient2.getFirstName() == "Aaron");
+        REQUIRE(patient2.getSurname() == "Carter");
+        REQUIRE(patient2.getAddress() == "address2");
+        REQUIRE(patient2.getEmail() == "email2");
+    }
 }
 
-TEST_CASE("patient2", "[patient2]")
-{
-  REQUIRE(patient2.getPatientID() == 2);
-  REQUIRE(patient2.getTitle() == "Mr.");
-  REQUIRE(patient2.getFirstName() == "Aaron");
-  REQUIRE(patient2.getSurname() == "Carter");
-  REQUIRE(patient2.getAddress() == "address2");
-  REQUIRE(patient2.getEmail() == "email2");
-}
+TEST_CASE("Appointment Initialization") {
+    SECTION("Appointment1 Initialization") {
+        REQUIRE(appointment1.getID() == 1);
+        REQUIRE(appointment1.getRoom() == &room1);
+        REQUIRE(appointment1.getDate() == now);
+        REQUIRE(appointment1.getDentist() == &dentist1);
+        REQUIRE(appointment1.getPatient() == &patient1);
+    }
 
-TEST_CASE("patient3", "[patient3]")
-{
-  REQUIRE(patient3.getPatientID() == 3);
-  REQUIRE(patient3.getTitle() == "Mr.");
-  REQUIRE(patient3.getFirstName() == "Brian");
-  REQUIRE(patient3.getSurname() == "Turner");
-  REQUIRE(patient3.getAddress() == "address3");
-  REQUIRE(patient3.getEmail() == "email3");
-}
+    SECTION("Appointment2 Initialization") {
+        REQUIRE(appointment2.getID() == 2);
+        REQUIRE(appointment2.getRoom() == &room2);
+        REQUIRE(appointment2.getDate() == now);
+        REQUIRE(appointment2.getDentist() == &dentist2);
+        REQUIRE(appointment2.getPatient() == &patient2);
+    }
 
-TEST_CASE("patient4", "[patient4]")
-{
-  REQUIRE(patient4.getPatientID() == 4);
-  REQUIRE(patient4.getTitle() == "Mr.");
-  REQUIRE(patient4.getFirstName() == "David");
-  REQUIRE(patient4.getSurname() == "Grey");
-  REQUIRE(patient4.getAddress() == "address4");
-  REQUIRE(patient4.getEmail() == "email4");
-}
 
-TEST_CASE("patient5", "[patient5]")
-{
-  REQUIRE(patient5.getPatientID() == 5);
-  REQUIRE(patient5.getTitle() == "Mr.");
-  REQUIRE(patient5.getFirstName() == "Gordon");
-  REQUIRE(patient5.getSurname() == "Pike");
-  REQUIRE(patient5.getAddress() == "address5");
-  REQUIRE(patient5.getEmail() == "email5");
-}
-
-TEST_CASE("room1", "[room1]")
-{
-  REQUIRE(room1.getID() == 1);
-}
-
-TEST_CASE("room2", "[room2]")
-{
-  REQUIRE(room1.getID() == 2);
-}
-
-TEST_CASE("room3", "[room3]")
-{
-  REQUIRE(room1.getID() == 3);
 }
